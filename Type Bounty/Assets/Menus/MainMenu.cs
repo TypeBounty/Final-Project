d895826517d4af2dbe47ac69c8443a91c7a4a7e2
TypeBounty <56374190+TypeBounty@users.noreply.github.com>
2019-11-20 19:07:25 -0800

    7:07

--- Type Bounty/Assets/Menus/MainMenu.cs
+++ Type Bounty/Assets/Menus/MainMenu.cs
@@ -11,7 +11,14 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
+    public void Learningcpp2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void Learningcpp3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
     public void QuitGame()
     {
         Debug.Log("QUIT!");
