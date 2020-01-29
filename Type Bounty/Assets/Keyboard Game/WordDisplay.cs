b701c385458308020686a04b6aeb871732f44af7
TypeBounty <56374190+TypeBounty@users.noreply.github.com>
2020-01-29 15:25:51 -0800

    11/29_Updated_Juan

--- Type Bounty/Assets/Keyboard Game/WordDisplay.cs
+++ Type Bounty/Assets/Keyboard Game/WordDisplay.cs
@@ -16,7 +16,7 @@ public class WordDisplay : MonoBehaviour
     public void RemoveLetter()
     {
         text.text = text.text.Remove(0, 1);
-        text.color = Color.red;
+        text.color = Color.green;
     }
 
     public void removeWord()
