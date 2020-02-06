b906c0863aa55101d5e7a1bc829820a4b548885a
TypeBounty <56374190+TypeBounty@users.noreply.github.com>
2020-02-06 11:50:02 -0800

    Revert "lesson & quizzes"
    This reverts commit fb3ddc06f2c278f9b5e6a3b570d6f0430a7a78ed.

---
+++ Type Bounty/Assets/C++/SCriptsforC++/nextbutton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class nextbutton : MonoBehaviour
+{
+    public static string resetAura = "n";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        //11-12
+        //transition to score
+        //11/19 update if statement to prevent it from transfering 
+        if (textcontrol.totalQuestions!=4 && textcontrol.trigger=="y")
+        {
+            textcontrol.randQuestion = -1;
+            resetAura = "y";
+            textcontrol.trigger = "n";
+        }
+        if (textcontrol.totalQuestions == 4 && textcontrol.trigger == "y")
+        {
+            SceneManager.LoadScene("ScoreScreen");
+        }
+    }
+
+}
+ 
