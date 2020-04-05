5a236ef43e549918abb705c79fb5ad4a3730bb4a
jchavez58 <46985768+jchavez58@users.noreply.github.com>
2020-04-05 15:55:12 -0700

    Quiz Updates

---
+++ Type Bounty/Assets/C++/Info C++ SCENES/QuizScripts/Answerbuttons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Answerbuttons : MonoBehaviour
+{
+    public Text answerText;
+    private AnswerData answerData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Setup(AnswerData data)
+    {
+        answerData = data;
+        answerText.text = answerData.answerText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
